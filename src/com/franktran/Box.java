ded06c3269dc5030c28d19bc50439b2f3a4d55c6
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-02-09 16:40:52 +0700

    Update

---
+++ src/com/franktran/Box.java
@@ -0,0 +1,25 @@
+package com.franktran;
+
+public class Box {
+
+    private Object contents;
+
+    void add(Object thing) {
+        if (contents == null) {
+            contents = thing;
+        } else {
+            System.out.println("The box is full.");
+        }
+    }
+
+    Object remove() {
+        if (contents == null) {
+            System.out.println("The box is empty.");
+            return null;
+        } else {
+            Object thing = contents;
+            contents = null;
+            return thing;
+        }
+    }
+}
---
+++ src/com/franktran/Oranges.java
@@ -0,0 +1,8 @@
+package com.franktran;
+
+class Oranges {
+
+    void juggle() {
+        System.out.println("You drop the oranges on the ground.");
+    }
+}
